30c3aa21d778047fc645cfa2efc4aa84916b474d
Alireza <ali.reza.mahmoud@gmail.com>
2020-06-11 22:01:43 +0200

    Edit - AppFixture - add loaduser

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,35 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->loadUser($manager);
+    }
 
+    public function __construct(
+        UserPasswordEncoderInterface $passwordEncoder
+    ) {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function loadUser(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('admin@admin.com');
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'admin123456'
+        ));
+        $manager->persist($user);
         $manager->flush();
     }
 }
