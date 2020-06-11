e605be205b9dc8d275bcb23d24a56afd758d2a49
Alireza <ali.reza.mahmoud@gmail.com>
2020-06-11 19:09:30 +0200

    Edit -Customer Entity- Email Validation

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CustomerRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 /**
@@ -60,6 +61,7 @@ class Customer
 
     /**
      * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\Email()
      */
     private $email;
 
