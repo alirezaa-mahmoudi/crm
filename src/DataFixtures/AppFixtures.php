1e21e2d5a44cf8a39ec21bb9df84f5561267cbd0
Alireza <ali.reza.mahmoud@gmail.com>
2020-06-11 23:32:31 +0200

    Edit - DataFixtures\AppFixture - add loadCustomer Function

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,24 +2,33 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Customer;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
     private $passwordEncoder;
 
+    /**
+     * @var Factory
+     */
+    private $faker;
+
     public function load(ObjectManager $manager)
     {
         $this->loadUser($manager);
+        $this->loadCustomer($manager);
     }
 
     public function __construct(
         UserPasswordEncoderInterface $passwordEncoder
     ) {
         $this->passwordEncoder = $passwordEncoder;
+        $this->faker = Factory::create();
     }
 
     public function loadUser(ObjectManager $manager)
@@ -31,6 +40,26 @@ class AppFixtures extends Fixture
             'admin123456'
         ));
         $manager->persist($user);
+        $manager->flush();
+    }
+
+    public function loadCustomer(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 50; ++$i) {
+            $customer = new Customer();
+            $customer->setEmail($this->faker->safeEmail);
+            $customer->setCity($this->faker->city);
+            $customer->setCompany($this->faker->company);
+            $customer->setFirstName($this->faker->firstName);
+            $customer->setLastName($this->faker->lastName);
+            $customer->setPhone($this->faker->phoneNumber);
+            $customer->setStreet($this->faker->streetAddress);
+            $customer->setZip($this->faker->postcode);
+            $customer->setUpdatedAt($this->faker->dateTimeThisYear);
+            $customer->setCountry($this->faker->country);
+
+            $manager->persist($customer);
+        }
         $manager->flush();
     }
 }
