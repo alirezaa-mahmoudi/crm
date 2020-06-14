559fceea6bd839c148e3590e1883eb7cfd992c6f
Alireza <ali.reza.mahmoud@gmail.com>
2020-06-14 17:17:00 +0200

    Edit CustomerTest - add testCustomerPutMethod

--- tests/CustomerTest.php
+++ tests/CustomerTest.php
@@ -3,11 +3,9 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\DependencyInjection\Container;
 
 class CustomerTest extends WebTestCase
 {
-
     public function login()
     {
         $client = self::createClient();
@@ -77,24 +75,42 @@ class CustomerTest extends WebTestCase
         $this->assertEquals(200, $response->getStatusCode());
     }
 
+
     public function testCustomerPostMethod()
     {
         $client = $this->login();
         $client->request('POST', '/api/customers/create', [], [], [],
-            '{	
-	                "email": "test@test.com",
-	                "company":"bmw",
-	                "phone": "017628245872",
-	                "country": "Germany",
-	                "firstName": "alirezaa",
-	                "lastName": "Mahmoudi",
-                	"street": "Kreuzberg",
-                    "city": "Berlin",
-                	"zip": "34645456"	
+            '{
+    	                "email": "test@test.com",
+    	                "company":"bmw",
+    	                "phone": "017628245872",
+    	                "country": "Germany",
+    	                "firstName": "alireza",
+    	                "lastName": "Mahmoudi",
+                	    "street": "Kreuzberg",
+                        "city": "Berlin",
+                	    "zip": "34645456"
                     }'
         );
         $this->assertEquals(201, $client->getResponse()->getStatusCode());
     }
 
+    public function testCustomerDeleteMethod()
+    {
+        $client = $this->login();
+        $client->request('DELETE', '/api/customers/delete/41');
+        $this->assertEquals(204, $client->getResponse()->getStatusCode());
+    }
 
+    public function testCustomerPutMethod()
+    {
+        $client = $this->login();
+        $client->request('PUT', '/api/customers/update/10', [], [], [],
+            '{	
+	                "country": "Iran"
+                    }'
+        );
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals('Iran', json_decode($client->getResponse()->getContent())->country);
+    }
 }
